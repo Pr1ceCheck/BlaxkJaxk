6aeb584b4942fde4f6ed93fb3764a3d40858043d
PR1CETAG <cprice@atl.yearup.og>
2022-10-30 23:07:39 -0400

    if else logic

--- javaScripts/jack.js
+++ javaScripts/jack.js
@@ -1,3 +1,13 @@
 let firstKard = 5;
 let secondKard = 11;
 let sum = firstKard + secondKard;
+//console.log(sum);
+if (sum < 21) {
+console.log("Do You Want to Draw Another Card ? ")
+} else if (sum === 21) {
+    console.log("Big Win !!, You've Got BlackJack")
+} else {
+    console.log("Oops You've Bust ")
+};
+
+
